706f13325a37551d4011a74d7d30ee0ceb376c36
Edivaldo Rangel <edivaldojcr@al.insper.edu.br>
2019-11-25 05:44:08 -0300

    Tudo funcionando pro primeiro elemento

--- assets/js/lista_uso.js
+++ assets/js/lista_uso.js
@@ -12,5 +12,71 @@ document.addEventListener('DOMContentLoaded', function(){
 		certo.classList.add('tem_pos')
 		errado.remove()
 	})
+	let mensagem =document.createElement('div')
+	let desisti = document.createElement('div')
+	let nachei = document.createElement('div')
+	let cancel = document.createElement('img')
+	let ok = document.createElement('div')
+
+
+	errado.addEventListener('click', function(){
+		mensagem.innerHTML = 'Por que deseja remover esse item da lista?'
+		mensagem.classList.add('mensagem')
+		body = document.querySelector('body')
+		body.appendChild(mensagem)
+		desisti.innerHTML = 'Desisti da compra.'
+		desisti.classList.add('desisti')
+		body.appendChild(desisti)
+		nachei.innerHTML = 'Não encontrei o produto.'
+		nachei.classList.add('nachei')
+		body.appendChild(nachei)
+		cancel.src="assets/img/nao_tem.png"
+		cancel.classList.add('cancel')
+		body.appendChild(cancel)
+	})
+
+	cancel.addEventListener('click',function(){
+		mensagem.remove()
+		cancel.remove()
+		desisti.remove()
+		nachei.remove()
+	})
+
+	desisti.addEventListener('click',function(){
+		mensagem.remove()
+		cancel.remove()
+		desisti.remove()
+		nachei.remove()
+		li.remove()
+		certo.remove()
+		errado.remove()
+	})
+
+	nachei.addEventListener('click',function(){
+		mensagem.innerHTML = 'A informação foi atualizada! Obrigado pela colaboração!'
+		cancel.remove()		
+		desisti.remove()
+		nachei.remove()
+		li.remove()
+		certo.remove()
+		errado.remove()
+		ok.innerHTML = 'Ok'
+		ok.classList.add('ok')
+		body.appendChild(ok)
+
+	})	
+
+	ok.addEventListener('click',function(){
+		mensagem.remove()
+		cancel.remove()
+		desisti.remove()
+		nachei.remove()
+		li.remove()
+		certo.remove()
+		errado.remove()
+		ok.remove()
+	})
+
+
 
 })	
