6aa66ce72d72ed6fd2e27bcf3c898fa14e469888
Edivaldo Rangel <edivaldojcr@al.insper.edu.br>
2019-11-28 20:17:15 -0300

    criando tela cheia e alguns links

---
+++ assets/js/resultado.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(){
+	let link = document.querySelector('.table')
+	link.addEventListener('click',function(){
+		window.location.href="lista_pao_de_acucar.html";	
+	})
+	let link2 = document.querySelector('.table1')
+	link2.addEventListener('click',function(){
+		window.location.href="lista_carrefour.html";	
+	})
+})	
