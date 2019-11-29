09b61f122a9233464c5a588449978476af5ac9eb
Edivaldo Rangel <edivaldojcr@al.insper.edu.br>
2019-11-29 08:53:12 -0300

    ..

---
+++ assets/js/obrigado.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+	let link = document.querySelector('.enviar')
+	link.addEventListener('click',function(){
+		window.location.href="obrigado.html";	
+	})
+})	
