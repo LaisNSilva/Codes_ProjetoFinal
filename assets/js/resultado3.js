9200006ff1ff6458238e8df4760f4134491d5370
Edivaldo Rangel <edivaldojcr@al.insper.edu.br>
2019-11-29 08:41:30 -0300

    ..

--- assets/js/resultado3.js
+++ assets/js/resultado3.js
@@ -5,6 +5,6 @@ document.addEventListener('DOMContentLoaded', function(){
 	})
 	let link2 = document.querySelector('.table1')
 	link2.addEventListener('click',function(){
-		window.location.href="lista_inutil.html";	
+		window.location.href="acucar_carrefour.html";	
 	})
 })	
