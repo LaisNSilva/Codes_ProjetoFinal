a9aa0ec24fb82e2c471db52d6eee9e165eef1608
Edivaldo Rangel <edivaldojcr@al.insper.edu.br>
2019-11-29 08:34:29 -0300

    .

---
+++ assets/js/resultado3.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(){
+	let link = document.querySelector('.table')
+	link.addEventListener('click',function(){
+		window.location.href="pao_de_acucar_acucar.html";	
+	})
+	let link2 = document.querySelector('.table1')
+	link2.addEventListener('click',function(){
+		window.location.href="lista_inutil.html";	
+	})
+})	
